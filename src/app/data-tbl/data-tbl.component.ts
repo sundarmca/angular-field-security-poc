e89a806e4c5894490d397221b7b16dfe73d885f6
sundarmca <techtween@gmail.com>
2020-04-26 16:37:24 +0530

    3 files modified

--- src/app/data-tbl/data-tbl.component.ts
+++ src/app/data-tbl/data-tbl.component.ts
@@ -9,12 +9,6 @@ import { map } from 'rxjs/operators';
 })
 export class DataTblComponent implements OnInit {
   results = [];
-  // lbel: string;
-  // name: string;
-  // team: string;
-  // project: string;
-  // inputval: string;
-  // property: string;
 
   formTitle = 'JSON response loaded to Data Table';
   constructor(private dtable: ForminputService) { 
@@ -32,26 +26,20 @@ export class DataTblComponent implements OnInit {
     this.fetchFromService()
     .subscribe((data: any): void => { 
         this.results = data.searchdata;
-        //  console.info(data);
-        // console.log(data['searchdata'][0]);
     });
   }
 
   onEditInit(event): void {
-    console.log(this.results);
-    console.log('Edit Init Event Called');
   }
 
   onEditCancel():void {
-    console.log(this.results);
-    console.log('Edit Cancel Event Called');
   }
 
-  // checkAccess(attr: any) {
-  //   if (attr === 'read' || attr === 'protected') {
-  //     return true;
-  //   }
-  // }
+  checkAccess(attr: any) {
+    if (attr === 'read' || attr === 'protected') {
+      return true;
+    }
+  }
 
   checkDisp(val: any, prop = null) {
     if (prop === 'protected') {
